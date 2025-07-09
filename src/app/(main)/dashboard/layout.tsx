829a3031028f5d17813429ae325b8affc94527da
Ankur <ankurpathak795@gmail.com>
2025-07-10 01:20:39 +0530

    changed layout.tsx to async for vercel

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-interface LayoutProps{
-    children : React.ReactNode;
-    params : Record<string, string>;
+interface LayoutProps {
+  children: React.ReactNode;
+  params: Record<string, string>;
 }
 
-const Layout:React.FC<LayoutProps> = ({children, params}) => {
+// ✅ Make it async if any dynamic segments are used in parent routes
+const Layout = async ({ children, params }: LayoutProps) => {
   return (
-    <main className='flex over-hidden h-screen'>
-        {children}
+    <main className="flex overflow-hidden h-screen">
+      {children}
     </main>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
